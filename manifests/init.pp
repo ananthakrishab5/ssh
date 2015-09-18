ed0430b82e6769a9b55424bf7fbd6bb287fbeda0
ananthakrishab5 <ananthakrishna.paturi@gmail.com>
2015-09-18 17:00:06 -0400

    Commit after test on slave

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class ssh{
+	package {'package':
+		name => 'openssh',
+		ensure => 'present',
+		}
+	file {'/var/tmp/ssh.txt':
+		ensure => 'file',
+		owner => 'root',
+		group => 'root',
+		source => 'puppet:///modules/ssh/ssh.txt',
+		require => Package['package'],
+		}
+	service {'sshd':
+		ensure => 'running',
+		enable => 'true',
+		subscribe => File['/var/tmp/ssh.txt'],
+		}
+}
